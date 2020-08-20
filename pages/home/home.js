5d0bc681a1c2ed169d1ae5e4d6df1261ff6db51b
asteryxxx <437767931@qq.com>
2020-08-20 23:24:31 +0800

    初见vant-weapp

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,35 @@
-// pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    imageURL:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1597946942689&di=a34abb6a2b67b7f8da0f445fe398dc21&imgtype=0&src=http%3A%2F%2Fwww.esys.cn%2Fuploads%2Fallimg%2F191001%2F1525213457_0.jpg",
+    value1: 0,
+    value2: 'a',
+    date: '',
+    show: false,
+    option1: [
+      { text: '全部商品', value: 0 },
+      { text: '新款商品', value: 1 },
+      { text: '活动商品', value: 2 },
+    ],
+    option2: [
+      { text: '默认排序', value: 'a' },
+      { text: '好评排序', value: 'b' },
+      { text: '销量排序', value: 'c' }]
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onDisplay() {
+    this.setData({ show: true });
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  onClose() {
+    this.setData({ show: false });
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  formatDate(date) {
+    date = new Date(date);
+    return `${date.getMonth() + 1}/${date.getDate()}`;
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  onConfirm(event) {
+    this.setData({
+      show: false,
+      date: this.formatDate(event.detail),
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
-})
+});
