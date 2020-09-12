1a25077d22311837c16475de518fd1cf89c8fdbb
asteryxxx <437767931@qq.com>
2020-09-13 00:10:07 +0800

    06_page注册页面

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,39 +1,58 @@
 // pages/home/home.js
 Page({
-
+  onPageScroll(obj){
+    console.log(obj);
+  },
+  onReachBottom(){
+    console.log('到达底部')
+  },
+  onPullDownRefresh(){
+    console.log("下拉刷新")
+  },
   /**
    * 页面的初始数据
    */
   data: {
-
+    message:"哈哈哈",
+    list:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log('onload')
+    wx.request({
+      url: 'http://123.207.32.32:8000/home/multidata',
+      success:(res)=>{
+        console.log(res)
+       const list = res.data.data.banner.list;
+       this.setData({
+         list:list
+       })
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    console.log('onReady')
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    console.log('onShow')
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    console.log('onHide')
   },
 
   /**
