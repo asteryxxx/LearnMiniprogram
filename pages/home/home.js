1ebcb3ec4a547f93d604206c06b9ebab56ee6c31
asteryxxx <437767931@qq.com>
2020-09-12 22:34:34 +0800

    03_知识点3

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,11 +1,26 @@
 // pages/home/home.js
 Page({
+  countevent:function(event){
+    console.log("被点击了。。。"+event)
+    // this.data.count += 1;
+    // 这个做不到数据响应式更新，要用setData才行
+    this.setData({
+      count: this.data.count + 1
+    })
+  },
 
   /**
    * 页面的初始数据
    */
   data: {
-
+      name:'lx',
+      age:18,
+      count:0,
+      stus:[
+        {id:110,name:'lx1',age:18},
+        {id:111,name:'lx2',age:28}
+        ,{id:112,name:'lx3',age:38},
+        {id:113,name:'lx5',age:48}  ]
   },
 
   /**
